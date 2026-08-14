12a55e0b583c1a071ceacf2b73a256adc531f46f
hadinull <hadinull.3000@gmail.com>
2026-08-15 00:27:15 +0300

    added spotify key

---
+++ src/Spotify.js
@@ -0,0 +1,32 @@
+let accessToken;
+const clientId = '';
+const redirectUri = 'http://localhost:5173/';
+
+const Spotify = {
+  async search(term) {
+    const url = 'https://spotifystefan-skliarovv1.p.rapidapi.com/search';
+    
+    const options = {
+      method: 'POST',
+      headers: { 
+        'x-rapidapi-key': import.meta.env.VITE_RAPIDAPI_KEY, /*key  */
+        'x-rapidapi-host': 'spotifystefan-skliarovv1.p.rapidapi.com',
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }, 
+      body: new URLSearchParams({
+        q: term, 
+        accessToken: '' 
+      })    
+    };
+
+    try {
+      const response = await fetch(url, options);
+      const result = await response.json(); 
+      console.log(result);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+};
+
+export default Spotify;
